0fa6c2555e2476b04031c1bed8d2b654bab0e8b4
Pavan Bethanabotla <pavan.kumar@infoservices.com>
2025-07-07 19:27:27 +0530

    Removed Duplicate AWS Provider

--- versions.tf
+++ versions.tf
@@ -1,11 +1,7 @@
 terraform {
   required_version = "~> 1.11.4" # Or your desired Terraform CLI version
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0" # Or your desired AzureRM provider version
-    }
+ 
     # Add other required providers here
   }
 
