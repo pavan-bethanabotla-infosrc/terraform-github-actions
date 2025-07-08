7f80e9f357f4a1e10ebf9fdd6de8c645864e7377
Pavan Bethanabotla <pavan.kumar@infoservices.com>
2025-07-08 14:02:43 +0530

    Added variables.tf file

---
+++ variables.tf
@@ -0,0 +1,13 @@
+# variables.tf
+
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID for authentication."
+  type        = string
+  sensitive   = true # Mark as sensitive to prevent showing in logs/outputs
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key for authentication."
+  type        = string
+  sensitive   = true # Mark as sensitive to prevent showing in logs/outputs
+}
